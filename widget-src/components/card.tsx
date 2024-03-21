01a19a3cd7015765ece004566bae29ddbb59fac5
pixbs <hey@pixbs.com>
2024-03-22 00:42:54 +0100

    ✅ Init UI

--- widget-src/components/card.tsx
+++ widget-src/components/card.tsx
@@ -24,12 +24,20 @@ function Card(props: CardProps) {
 	function handleVerMovement(index: number) {
 		setBoard(moveCard(board, props, columnIndex, index))
 	}
+
+	async function handleClick() {
+		await new Promise ((resolve) => {
+			figma.showUI(__html__)
+		})
+	}
+	
 	return (
 		<AutoLayout
 			direction="vertical"
 			padding={8}
 			fill="#fff"
 			key={id}
+			onClick={handleClick}
 		>
 			<Text>{name}</Text>
 			<Button text="Move left" onClick={() => handleHorMovement(columnIndex - 1)} />
